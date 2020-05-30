f45819147d8758a901557113bc47630d77323319
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-05-30 17:19:30 +0200

    Chore: Fixed eslint

--- .babelrc.js
+++ .babelrc.js
@@ -2,7 +2,6 @@ module.exports = {
 	presets: ['@babel/preset-env'],
 	plugins: [
 		'@babel/plugin-transform-modules-commonjs',
-		'@babel/plugin-syntax-export-default-from',
 		'@babel/plugin-proposal-export-default-from',
 		[
 			'babel-plugin-root-import',
