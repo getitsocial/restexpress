62c2c766ca1f7e3e73e9805e09a64acf16dac35d
Tayfun Gülcan <tayfunmobile@gmail.com>
2020-05-31 21:46:35 +0200

    Fix: Bullshit deleted

---
+++ src/api/message/controller.js
@@ -0,0 +1,63 @@
+import { merge } from 'lodash'
+import { success, notFound } from 's/response'
+import { Message } from '.'
+
+// Get
+export const getAll = async (
+	{ querymen: { query, select, cursor } },
+	res,
+	next
+) => {
+	try {
+		const messages = await Message.find(query, select, cursor)
+		console.log(messages)
+		await success(res)(messages.map(message => message.view()))
+	} catch (error) {
+		next(error)
+	}
+}
+
+// Get One
+export const getOne = async ({ params: { id } }, res, next) => {
+	try {
+		const message = await Message.findById(id)
+		await notFound(res)(message)
+		await success(res)(message ? message.view() : null)
+	} catch (error) {
+		next(error)
+	}
+}
+
+// Post
+export const create = async ({ bodymen: { body } }, res, next) => {
+	try {
+		const message = await Message.create(body)
+		await success(res, 201)(message.view())
+	} catch (error) {
+		next(error)
+	}
+}
+
+// Put
+export const update = async ({ bodymen: { body }, params }, res, next) => {
+	try {
+		const message = await Message.findById(params.id)
+		await notFound(res)(message)
+		message => (message ? merge(message, body).save() : null)
+		await success(res)(message ? message.view(true) : null)
+	} catch (error) {
+		next(error)
+	}
+}
+
+// Delete
+export const destroy = async ({ params: { id } }, res, next) => {
+	try {
+		const message = await Message.findById(id)
+		await notFound(res)(message)
+		message ? await message.remove() : null
+		await success(res, 204)(message)
+	} catch (error) {
+		next(error)
+	}
+}
