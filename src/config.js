ff98045f90eb93a0609e381fc3e66348aad82c3f
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-06-03 10:33:13 +0200

    added bugsnag

--- src/config.js
+++ src/config.js
@@ -35,6 +35,9 @@ const config = {
 		rateLimiter: {
 			windowMs: 15 * 60 * 1000, // 15 minutes
 			max: 100 // limit each IP to 100 requests per windowMs
+		},
+		bugsnag: {
+			secret: requireProcessEnv('BUGSNAG_API_KEY')
 		}
 	},
 	test: {},
