4084d046745e45e24db7a757335e8c05a0013003
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-06-09 14:58:31 +0200

    WIP CASL

--- src/api/message/ability.js
+++ src/api/message/ability.js
@@ -1,5 +1,8 @@
 import { AbilityBuilder, Ability } from '@casl/ability'
 
+const isAdmin = user => user?.role === 'admin'
+const isUser = user => user?.role === 'user'
+
 /**
  * @param user contains details about logged in user: its id, name, email, etc
  */
@@ -9,13 +12,21 @@ const defineAbilitiesFor = user => {
 	// can read blog posts
 	can('read', 'Message', ['id', 'content'])
 
+	if (isUser(user)) {
+		can('update', 'Message', { author: user._id })
+	}
+
+	if (isAdmin(user)) {
+		can(['read', 'update', 'delete'], 'Message')
+	}
+
 	return new Ability(rules)
 }
 
 const ANONYMOUS_ABILITY = defineAbilitiesFor(null)
 
 const createAbilities = (req, res, next) => {
-	req.ability = req.user.email
+	req.ability = req.user?.jti
 		? defineAbilitiesFor(req.user)
 		: ANONYMOUS_ABILITY
 	next()
