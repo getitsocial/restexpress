b0bfa72ae3c856228e31d4485932a49d8d00e33f
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-06-20 11:35:15 +0200

    Test: 'fix' test execution

--- src/server.js
+++ src/server.js
@@ -2,13 +2,15 @@ import { env, mongo, port, ip, apiRoot } from '~/config'
 import mongoose from 's/mongoose'
 import express from 's/express'
 import api from 'a/'
+const processMode =  process.env.NODE_ENV
 
 const app = express(apiRoot, api)
+if (processMode !== 'test') {
+    mongoose.connect(mongo.uri)
 
-mongoose.connect(mongo.uri)
-
-app.listen(port, () => {
-	console.log(`Server started on ${ip}:${port}`)
-})
+    app.listen(port, () => {
+        console.log(`Server started on ${ip}:${port}`)
+    })
+}
 
 export default app
