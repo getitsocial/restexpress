b6b2e436bdb79205cbb9e51e4a6cb2f98ce552b7
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-06-27 21:37:24 +0200

    Fix: remove test file

--- test.js
+++
@@ -1,8 +0,0 @@
-const randtoken = require('rand-token');
-const arr = [];
-const findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index)
-
-for (let i = 0; i < 100000; i+=1) {
-	arr.push(randtoken.uid(32))
-}
-console.log(findDuplicates(arr));
