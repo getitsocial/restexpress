0495190fc079b4d48f04fd88787de9ecace2e506
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-06-29 19:41:37 +0200

    Docs: Added swagger docs to user

--- src/services/express/swagger.js
+++ src/services/express/swagger.js
@@ -2,12 +2,15 @@ import { Router } from 'express'
 import swaggerJSDoc from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
 import { Message } from '~/api/message'
+import { User } from '~/api/user'
 import { swagger } from '~/config'
 
 const specs = swaggerJSDoc(swagger)
 const router = new Router()
-
-Object.assign(specs.components.schemas, {Message: Message.swaggerSchema})
+// TODO: Dynamic imports maybe?
+specs.components.schemas = {}
+Object.assign(specs.components.schemas, { Message: Message.swaggerSchema })
+Object.assign(specs.components.schemas, { User: User.swaggerSchema })
 
 // console.log(specs)
 router.use(swagger.url, swaggerUi.serve)
