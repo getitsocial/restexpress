d073c593e1cd9e1dcf08e67e1c0af7948b4bceff
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-07-03 17:27:41 +0200

    Docs: Added authentication doc

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -3,21 +3,61 @@ import { middleware as body } from 'bodymen'
 import { authenticate, providerAuthenticate, logout, logoutAll } from './controller'
 import { masterman, doorman } from '~/services/auth'
 
+/**
+ * @swagger
+ * tags:
+ *   name: Authentication
+ *   description: Authenticate a user
+ */
 const router = new Router()
 
 /**
- * @api {post} /auth Authenticate
- * @apiName Authenticate
- * @apiGroup Auth
- * @apiPermission master
- * @apiHeader {String} Authorization Basic authorization with email and password.
- * @apiParam {String} access_token Master access_token.
- * @apiSuccess (Success 201) {String} token User `access_token` to be passed to other requests.
- * @apiSuccess (Success 201) {Object} user Current user's data.
- * @apiError 401 Master access only or invalid credentials.
+ * @swagger
+ * path:
+ *  api/auth/:
+ *    post:
+ *      summary: Login
+ *      tags: [Authentication]
+ *      security:
+ *        - masterKey: []
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                email:
+ *                  type: string
+ *                  format: email
+ *                password:
+ *                  type: string
+ *      responses:
+ *        "200":
+ *          description: A user schema
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  _id:
+ *                    type: string
+ *                  role:
+ *                    type: string
+ *                  token:
+ *                    type: string
+ *        "400":
+ *          description: Invalid Body
+ *        "401":
+ *          description: Missing masterkey
+ *        "403":
+ *          description: Missing permissions (user or password wrong or user is not verified)
+ *        "500":
+ *          description: Oh boi
  */
 router.post(
     '',
+    masterman(),
     body({
         email: {
             type: String,
@@ -28,28 +68,95 @@ router.post(
             required: true
         }
     }),
-    masterman(),
     authenticate
 )
 
 /**
- * @api {post} /auth/logout logout current user
- * @apiName LogoutUser
- * @apiGroup Auth
- * @apiError 401 Invalid credentials.
+ * @swagger
+ * path:
+ *  api/auth/logout:
+ *    post:
+ *      summary: Logout the current session
+ *      tags: [Authentication]
+ *      security:
+ *        - jwtSessionToken: []
+ *      responses:
+ *        "204":
+ *          description: Success
+ *        "400":
+ *          description: Token missing
+ *        "500":
+ *          description: Oh boi
  */
 router.post('/logout', logout)
 
+/**
+ * @swagger
+ * path:
+ *  api/auth/logout/all:
+ *    post:
+ *      summary: Logout all sessions which are associated with the user
+ *      tags: [Authentication]
+ *      security:
+ *        - jwtSessionToken: []
+ *      responses:
+ *        "204":
+ *          description: Success
+ *        "400":
+ *          description: Token missing
+ *        "500":
+ *          description: Oh boi
+ */
 router.post('/logout/all', logoutAll)
 
+
+
 /**
- * @api {post} /auth/:provider Authenticate with Facebook or Google
- * @apiName AuthenticateProvider
- * @apiGroup Auth
- * @apiParam {String} access_token Facebook or Google user accessToken.
- * @apiSuccess (Success 201) {String} token User `access_token` to be passed to other requests.
- * @apiSuccess (Success 201) {Object} user Current user's data.
- * @apiError 401 Invalid credentials.
+ * @swagger
+ * path:
+ *  api/auth/{provider}:
+ *    post:
+ *      summary: Login with 3rd party
+ *      tags: [Authentication]
+ *      parameters:
+ *        - in: path
+ *          name: provider
+ *          schema:
+ *            type: string
+ *            enum: [facebook, google]
+ *          required: true
+ *          description: 3rd party provider name
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                token:
+ *                  type: string
+ *      responses:
+ *        "200":
+ *          description: A user schema
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  _id:
+ *                    type: string
+ *                  role:
+ *                    type: string
+ *                  token:
+ *                    type: string
+ *        "400":
+ *          description: Invalid Body
+ *        "401":
+ *          description: Missing masterkey
+ *        "403":
+ *          description: Missing permissions (user or password wrong or user is not verified)
+ *        "500":
+ *          description: Oh boi
  */
 router.post(
     '/:provider',
