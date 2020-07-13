17c411010e2acef0658842d5bef6bc1e05802479
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-07-13 12:11:24 +0200

    Refactor: Remove messages endpoint

--- src/api/acl.js
+++ src/api/acl.js
@@ -1,6 +1,5 @@
-import { merge, flow, groupBy, mapValues, map, omit } from 'lodash'
+import { groupBy } from 'lodash'
 /* ENDPOINT_ACL_IMPORT */
-import messageAcl from './message/acl'
 import authAcl from './auth/acl'
 import userAcl from './user/acl'
 import verificationAcl from './verification/acl'
@@ -12,7 +11,6 @@ const permissions = {
     ...groupBy([
         /* ENDPOINT_ACL_EXPORT */
         ...defaultPermissions,
-        ...messageAcl,
         ...authAcl,
         ...userAcl,
         ...verificationAcl,
--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,9 @@
 import { Router } from 'express'
-import { env, mongo, port, ip, apiRoot } from '~/config'
-import { doorman } from '~/services/auth/guard'
 /* ENDPOINT_ROUTER_IMPORT */
 import auth from './auth'
 import verification from './verification'
 import passwordReset from './password-reset'
 import user, { User } from './user'
-import message, { Message } from './message'
 
 const router = new Router()
 
@@ -14,14 +11,12 @@ const router = new Router()
 router.use('/auth', auth)
 router.use('/verification', verification)
 router.use('/users', user)
-router.use('/messages', message)
 router.use('/password-reset', passwordReset)
 
 // Export the relevant models for swagger documentation
 export const Models = [
     /* ENDPOINT_DOCS_EXPORT */
-    User,
-    Message
+    User
 ]
 
 // Export router for Express Server
